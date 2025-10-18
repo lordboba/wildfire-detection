4375ce5745cf7220ff0c18106080b04840834c87
Tyrol Xiao <yxiao1717@gmail.com>
2025-10-18 02:19:27 -0700

    Add data cleaning script

---
+++ dataclean.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+df = pd.read_csv("data/wildfire_aqi_dataset.csv")
+
+smoke_to_float = {
+    "none": 0.0,
+    "light": 1.0,
+    "medium": 1.0,
+    "heavy": 1.0,
+}
+
+df["status"] = (
+    df["status"]
+    .str.lower()
+    .map(smoke_to_float)
+    .fillna(0.0)  # optional fallback if unexpected labels appear
+)
+
+df.to_csv("data/wildfire_aqi_dataset_clean.csv", index=False)
